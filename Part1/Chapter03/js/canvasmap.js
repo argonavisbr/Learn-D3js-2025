7b8d8a941b9862d700e3bfaea8b3a7339c829749
helderdarocha <helder.darocha@gmail.com>
2023-05-16 12:19:30 -0300

    Updated files

--- Part1/Chapter03/js/canvasmap.js
+++ Part1/Chapter03/js/canvasmap.js
@@ -11,7 +11,7 @@ function drawPolygon(coords) {
     for(let i = 0; i < coords.length; i++ ) {
         let latitude = coords[i][1];
         let longitude = coords[i][0];
-        if(i == 0) {
+        if(i === 0) {
             ctx.moveTo(scaleX(longitude), scaleY(latitude));
         } else {
             ctx.lineTo(scaleX(longitude), scaleY(latitude));
@@ -23,9 +23,9 @@ function drawPolygon(coords) {
 
 function drawMap(data) {
     data.forEach(obj => {
-        if(obj.geometry.type == 'MultiPolygon') {
+        if(obj.geometry.type === 'MultiPolygon') {
             obj.geometry.coordinates.forEach(poly => drawPolygon(poly[0]));
-        } else if(obj.geometry.type == 'Polygon') {
+        } else if(obj.geometry.type === 'Polygon') {
             obj.geometry.coordinates.forEach(poly => drawPolygon(poly));
         }
     });
