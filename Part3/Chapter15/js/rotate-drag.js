879381914a369672fe6c49fdd4bcd620a57c865f
helderdarocha <helder.darocha@gmail.com>
2025-04-17 22:47:32 -0300

    Refactor file structure and update D3 hierarchy methods for consistency

---
+++ Part3/Chapter15/js/rotate-drag.js
@@ -0,0 +1,26 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7";
+
+// Very simple draggable rotation
+export function addRotateDrag(chart, dim) {
+    const state = {r: 0, x: dim.width/2, y: dim.height/2}
+
+    chart.attr("transform", `translate(${[dim.width / 2, dim.height / 2]}) rotate(${state.r})`);
+
+    chart.call(d3.drag()
+        .on("start", function(evt) {
+            const ctm = this.getCTM();
+            const x = evt.x - ctm.e;
+            const y = evt.y - ctm.f;
+            state.startAngle = Math.atan2(y, x) * 180 / Math.PI;
+        })
+        .on("drag", function(evt) {
+            const ctm = this.getCTM();
+            const x = evt.x - ctm.e;
+            const y = evt.y - ctm.f;
+            const currentAngle = Math.atan2(y, x) * 180 / Math.PI;
+            state.r += currentAngle - state.startAngle;
+            state.startAngle = currentAngle;
+            chart.attr("transform", `translate(${[dim.width / 2, dim.height / 2]}) rotate(${state.r})`);
+        }));
+}
+
